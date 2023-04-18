0b7393f394ed757083cda994cc581cd784cae0e1
juanxo13597 <juanxo97master@gmail.com>
2023-04-18 11:46:04 +0200

    bootstrap init

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import serverless = require('serverless-http');
 import * as express from 'express';
 import { join } from 'path';
 
@@ -8,13 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use('/public', express.static(join(__dirname, '..', 'public')));
-  await app.init();
-  const expressApp = app.getHttpAdapter().getInstance();
-  return serverless(expressApp);
+  await app.listen(process.env.PORT || 3000);
 }
-
-let server;
-export const handler = async (event, context, callback) => {
-  server = server ?? (await bootstrap());
-  return server(event, context, callback);
-};
+bootstrap();
